b094f1337c18850b6b2092659009163968b0108d
ernestocruzm <ernestocruzmanjarrez@gmail.com>
2021-10-03 23:08:40 -0500

    segunda carga

--- AppsInventory/Controllers/WeatherForecastController.cs
+++ AppsInventory/Controllers/WeatherForecastController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace AppsInventory.Controllers
 {
+    //test
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
